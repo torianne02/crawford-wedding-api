0b50a33c094dd5c34b6e436ba25048a92b50fbcb
torianne02 <victoria.fluharty@gmail.com>
2019-01-28 15:39:20 -0800

    comments out guests_controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,46 +1,48 @@
-class GuestsController < ApplicationController
-  before_action :set_guest, only: %i[show]
+# FUTURE APP USE
 
-  # GET /guests/
-  def index
-    @guests = Guest.all
-    render json: @guests
-  end
-
-  # GET /guests/:id
-  def show
-    @guest_rsvp = @guest.rsvp
-    response = { :guest => @guest, :guest_rsvp => @guest_rsvp }
-    render json: response
-  end
-
-  def new
-    @guest = Guest.new
-    @rsvp = Rsvp.new
-  end
-
-  # POST /guests
-  def create
-    @guest = Guest.find_or_create_by(guest_params)
-    @rsvp = Rsvp.create(guest_params[:rsvp_attributes])
-    @rsvp.guest_id = @guest.id
-
-    if @guest.save
-      response json: @guest
-    end
-  end
-
-  private
-
-  def set_guest
-    @guest = Guest.find_by(id: params[:id])
-  end
-
-  def guest_params
-    params.require(:guest).permit(
-      :name,
-      :email,
-      rsvp_attributes: %i[attendees song_request accept]
-    )
-  end
-end
+# class GuestsController < ApplicationController
+#   before_action :set_guest, only: %i[show]
+#
+#   # GET /guests/
+#   def index
+#     @guests = Guest.all
+#     render json: @guests
+#   end
+#
+#   # GET /guests/:id
+#   def show
+#     @guest_rsvp = @guest.rsvp
+#     response = { :guest => @guest, :guest_rsvp => @guest_rsvp }
+#     render json: response
+#   end
+#
+#   def new
+#     @guest = Guest.new
+#     @rsvp = Rsvp.new
+#   end
+#
+#   # POST /guests
+#   def create
+#     @guest = Guest.find_or_create_by(guest_params)
+#     @rsvp = Rsvp.create(guest_params[:rsvp_attributes])
+#     @rsvp.guest_id = @guest.id
+#
+#     if @guest.save
+#       response json: @guest
+#     end
+#   end
+#
+#   private
+#
+#   def set_guest
+#     @guest = Guest.find_by(id: params[:id])
+#   end
+#
+#   def guest_params
+#     params.require(:guest).permit(
+#       :name,
+#       :email,
+#       rsvp_attributes: %i[attendees song_request accept]
+#     )
+#   end
+# end
