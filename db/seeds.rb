509b8c6fd0a2256501106bc625ad0db2d526b3db
torianne02 <victoria.fluharty@gmail.com>
2019-01-29 13:16:10 -0800

    adds seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Rsvp.create(name: 'Malinda Flu', email: 'mflu@aol.com', accept: true, attendees: 2, song_request: 'Something Like This - The Chainsmokers')
+Rsvp.create(name: 'Daniel Flu', email: 'dflu@aol.com', accept: true, attendees: 3, song_request: 'High - Alison Wonderland')
+Rsvp.create(name: 'Dee Leebove', email: 'deelee@aol.com', accept: true, attendees: 2, song_request: 'You - Galantis')
+Rsvp.create(name: 'Georgie Mitch', email: 'gmitch@aol.com', accept: true, attendees: 3, song_request: 'Happens Like That - Granger Smith')
+Rsvp.create(name: 'Donna Wright', email: 'dwright@aol.com', accept: false, attendees: 0, song_request: '')
